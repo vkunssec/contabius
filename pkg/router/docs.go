36448c12d82cef66a6f056d17cc79ce82560627c
Victor <vkunssec@gmail.com>
2024-04-23 00:11:14 -0300

    feature(docs): doc router

---
+++ pkg/router/docs.go
@@ -0,0 +1,7 @@
+package router
+
+import "github.com/gofiber/fiber/v2"
+
+func DocsRoutes(app fiber.Router) {
+	app.Static("/docs", "docs/public")
+}
--- pkg/router/routes.go
+++ pkg/router/routes.go
@@ -3,5 +3,6 @@ package router
 import "github.com/gofiber/fiber/v2"
 
 func Routes(app fiber.Router) {
+	DocsRoutes(app)
 	AccountRoutes(app)
 }
