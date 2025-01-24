718e8a82bcd1c122ec49a7d6a68af9588eba3887
Victor Martins <vkunssec@gmail.com>
2025-01-23 22:26:31 -0300

    docs: comments in structs

--- pkg/domain/Accounts.go
+++ pkg/domain/Accounts.go
@@ -2,10 +2,11 @@ package domain
 
 import "time"
 
+// Accounts é uma estrutura que representa uma conta
 type Accounts struct {
-	Id        string    `json:"id,omitempty" bson:"_id,omitempty" example:"678079f6f5080a39a8eedc1e"`
-	Account   string    `json:"account,omitempty" bson:"account,omitempty" example:"Conta Corrente"`
-	Color     string    `json:"color,omitempty" bson:"color,omitempty" example:"#000000"`
-	CreatedAt time.Time `json:"created_at,omitempty" bson:"created_at,omitempty" example:"2025-01-01T00:00:00Z"`
-	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty" example:"2025-01-01T00:00:00Z"`
+	Id        string    `json:"id,omitempty" bson:"_id,omitempty" example:"678079f6f5080a39a8eedc1e"`            // ID da conta
+	Account   string    `json:"account,omitempty" bson:"account,omitempty" example:"Conta Corrente"`             // Nome da conta
+	Color     string    `json:"color,omitempty" bson:"color,omitempty" example:"#000000"`                        // Cor da conta
+	CreatedAt time.Time `json:"created_at,omitempty" bson:"created_at,omitempty" example:"2025-01-01T00:00:00Z"` // Data de criação
+	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty" example:"2025-01-01T00:00:00Z"` // Data de atualização
 }
--- pkg/domain/Categories.go
+++ pkg/domain/Categories.go
@@ -2,10 +2,11 @@ package domain
 
 import "time"
 
+// Categories é uma estrutura que representa uma categoria
 type Categories struct {
-	Id        string    `json:"id,omitempty" bson:"_id,omitempty" example:"678079f6f5080a39a8eedc1e"`
-	Category  string    `json:"category" bson:"category" example:"Alimentação"`
-	Parent    *string   `json:"parent" bson:"parent" example:"678079f6f5080a39a8eedc1e"`
-	CreatedAt time.Time `json:"created_at,omitempty" bson:"created_at,omitempty" example:"2025-01-01T00:00:00Z"`
-	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty" example:"2025-01-01T00:00:00Z"`
+	Id        string    `json:"id,omitempty" bson:"_id,omitempty" example:"678079f6f5080a39a8eedc1e"`            // ID da categoria
+	Category  string    `json:"category" bson:"category" example:"Alimentação"`                                  // Nome da categoria
+	Parent    *string   `json:"parent" bson:"parent" example:"678079f6f5080a39a8eedc1e"`                         // ID do pai da categoria
+	CreatedAt time.Time `json:"created_at,omitempty" bson:"created_at,omitempty" example:"2025-01-01T00:00:00Z"` // Data de criação
+	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty" example:"2025-01-01T00:00:00Z"` // Data de atualização
 }
