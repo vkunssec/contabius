f98c1c9b21f974607cd417d57550f9fb3d111025
Victor Martins <vkunssec@gmail.com>
2025-01-25 15:38:37 -0300

    chore: add account in investments

--- pkg/domain/Investments.go
+++ pkg/domain/Investments.go
@@ -5,6 +5,7 @@ import "time"
 type Investments struct {
 	Investment string    `json:"investment" bson:"investment"`
 	Amount     Money     `json:"amount" bson:"amount"`
+	Account    Accounts  `json:"account" bson:"account"`
 	CreatedAt  time.Time `json:"created_at" bson:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at" bson:"updated_at"`
 }
